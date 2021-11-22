59e686a994ed331c2a713186935bd1d5387e767b
Jack Pan <panmx9909@gmail.com>
2021-11-22 13:01:39 +0800

    restructure

---
+++ PSA/Ising/vec2Unitary.py
@@ -0,0 +1,38 @@
+import jax
+import jax.numpy as jnp
+
+
+def vec2Unitary(N,input):
+    '''
+    Converts unitary matrix to a vector of parameters (which parameterize the unitary). This vector effectively parameterizes SU(4).
+
+
+    Args:
+        N(int): lattice size
+        input(float arrary): 2^(2N)-1 size free parameters
+
+    Returns:
+        A complex (2^N,2^N) array that parameterized by the input vector
+    '''
+    mat_size = int(2**N)
+
+    # make sure the trace is 0
+    vec = jnp.append(input,-jnp.sum(input[-mat_size+1:]))
+
+    h_mat = jnp.diag(vec[-mat_size:])
+
+
+    count = 0
+    #real part
+    for i in range(1,mat_size):
+        h_mat += jnp.diag(vec[count:count+i],k=-mat_size+i)
+        count += i
+
+    #img part
+    for i in range(1,mat_size):
+        h_mat += jnp.diag(1j * vec[count:count+i],k=-mat_size+i)
+        count += i
+
+    h_mat = (h_mat.conjugate().T + h_mat)/2
+    unitary = jax.scipy.linalg.expm(1j * h_mat)
+    return unitary
